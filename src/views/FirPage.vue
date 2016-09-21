d415aaf29b861411f1fbaa3bbfe388c3faffff6e
zhongjx <bjzhongjingxue@corp.netease.com>
2016-09-21 17:14:25 +0800

    simplify the demo

---
+++ src/views/FirPage.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="page">
+        <h1>首页</h1>
+        <h3>{{msg}}</h3>
+        <input type="button" v-touch:tap="showModal" value="显示遮罩层"/>
+        <list></list>
+        <timer></timer>
+        <modal></modal>
+
+    </div>
+</template>
+<script>
+    import List from './List.vue'
+    import Timer from './Timer.vue'
+    import Modal from './Modal.vue'
+
+    export default {
+        data: function(){
+            return {
+                msg: '放置首页相关组件'
+            }
+        },
+        components:{
+            List, Timer, Modal
+        },
+        methods:{
+            'showModal': function(){
+                this.$broadcast('show-modal')
+            }
+        }
+    }
+</script>
+<style lang="sass" scoped>
+    $red:red;
+    h1{
+        background: $red;
+    }
+    input{
+        border: 1px solid #eff1f5;
+        padding: 10px;
+        height: 50px;
+        border-radius: 10px;
+    }
+    input:hover{
+        font-weight: bolder;
+    }
+</style>
+
--- src/views/SecPage.vue
+++ src/views/SecPage.vue
@@ -2,20 +2,15 @@
     <div class="page">
         <h1>第二页</h1>
         <p>放置第二页相关组件</p>
-        <test></test>
     </div>
 </template>
 <script>
-    import Test from './Test.vue'
 
     export default {
         data: function () {
             return {
                 msg: 'This is a modal!'
             }
-        },
-        components:{
-            test:Test
         }
     }
 </script>
